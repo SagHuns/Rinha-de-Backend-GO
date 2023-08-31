0f6362cf26b470dfe63be3a076b65021a3323eca
Lucas Soares <lucascsoares21@gmail.com>
2023-08-31 14:12:26 -0300

    Third endpoint created

--- main.go
+++ main.go
@@ -23,73 +23,80 @@ var Database []Pessoa  // Cria DB simples de pessoas
 func main() {
     PORT := ":8080"
     log.Print("Server initialized at port ", PORT)
-    http.HandleFunc("/pessoas", pessoasPostHandler)  // Criando um caminho para localhost:8080/pessoas/
-    http.HandleFunc("/pessoas/", pessoasGetHandler) // GET handler para pessoas com id
+    http.HandleFunc("/pessoas", pessoasHandler)
+    http.HandleFunc("/pessoas/", pessoasGetHandler)
     log.Fatal(http.ListenAndServe(PORT, nil))  // Ouve os endpoints
 }
 
+
+// Um meio para saber se o /pessoas é para fazer uma busca e dispôr atributos
+// ou se é para fazer um POST
+func pessoasHandler(w http.ResponseWriter, r *http.Request) {
+    if r.Method == http.MethodGet {
+        pessoasSearchHandler(w, r)
+
+    } else if r.Method == http.MethodPost {
+        pessoasPostHandler(w, r)
+    }
+}
 func pessoasPostHandler(w http.ResponseWriter, r *http.Request) {
     w.Header().Set("Content-Type", "application/json; charset=utf-8")  // Mudando o formato principal de envio para JSON
 
-    if r.Method == http.MethodPost {
-        var PessoaJson Pessoa
-        const nascimento_valido = "2006-01-02"
+    var PessoaJson Pessoa
+    const nascimento_valido = "2006-01-02"
 
-        var error1 error = json.NewDecoder(r.Body).Decode(&PessoaJson)  // Caso ocorra um erro na hora de decodificar o JSON
+    var error1 error = json.NewDecoder(r.Body).Decode(&PessoaJson)  // Caso ocorra um erro na hora de decodificar o JSON
 
-        if error1 != nil {
-            w.WriteHeader(http.StatusBadRequest)
-            return
-        }
-
-        if PessoaJson.Apelido == "" {
-            w.WriteHeader(http.StatusUnprocessableEntity)
-            w.Write([]byte("Digite um Apelido não nulo!"))
-            return
-        }
-
-        for _, pessoa := range Database {
-            if pessoa.Apelido == PessoaJson.Apelido {
-                w.WriteHeader(http.StatusBadRequest)
-                w.Write([]byte("Apelido já existe!"))
-                return
-            }
-        }
-
-        if PessoaJson.Nome == "" {
-            w.WriteHeader(http.StatusUnprocessableEntity)
-            w.Write([]byte("Digite um Nome não nulo!"))
-            return
-        }
-
-        if PessoaJson.Nascimento == "" {
-            w.WriteHeader(http.StatusUnprocessableEntity)
-            w.Write([]byte("Digite um Nascimento não nulo!"))
-            return
-        }
-
-        _, erro := time.Parse(nascimento_valido, PessoaJson.Nascimento)
-        if erro != nil {
-            w.WriteHeader(http.StatusUnprocessableEntity)
-            w.Write([]byte("Digite a data de nascimento no formato AAAA-MM-DD"))
-            return
-        }
-
-        id := uuid.New()
-        // Cria url com esse UUID
-        url := "/pessoas/" + id.String()
-        PessoaJson.Id = id
-        Database = append(Database, PessoaJson)
-
-        // Write the created URL to the response header
-        w.Header().Set("Location", url)
-
-        w.WriteHeader(http.StatusCreated)
-        json.NewEncoder(w).Encode(PessoaJson)
-
-    } else {
-        w.WriteHeader(http.StatusNotFound)
+    if error1 != nil {
+        w.WriteHeader(http.StatusBadRequest)
+        return
     }
+
+    if PessoaJson.Apelido == "" {
+        w.WriteHeader(http.StatusUnprocessableEntity)
+        w.Write([]byte("Digite um Apelido não nulo!"))
+        return
+    }
+
+    for _, pessoa := range Database {
+        if pessoa.Apelido == PessoaJson.Apelido {
+            w.WriteHeader(http.StatusBadRequest)
+            w.Write([]byte("Apelido já existe!"))
+            return
+        }
+    }
+
+    if PessoaJson.Nome == "" {
+        w.WriteHeader(http.StatusUnprocessableEntity)
+        w.Write([]byte("Digite um Nome não nulo!"))
+        return
+    }
+
+    if PessoaJson.Nascimento == "" {
+        w.WriteHeader(http.StatusUnprocessableEntity)
+        w.Write([]byte("Digite um Nascimento não nulo!"))
+        return
+    }
+
+    _, erro := time.Parse(nascimento_valido, PessoaJson.Nascimento)
+    if erro != nil {
+        w.WriteHeader(http.StatusUnprocessableEntity)
+        w.Write([]byte("Digite a data de nascimento no formato AAAA-MM-DD"))
+        return
+    }
+
+    id := uuid.New()
+    // Cria url com esse UUID
+    url := "/pessoas/" + id.String()
+    PessoaJson.Id = id
+    Database = append(Database, PessoaJson)
+
+    // Write the created URL to the response header
+    w.Header().Set("Location", url)
+
+    w.WriteHeader(http.StatusCreated)
+    json.NewEncoder(w).Encode(PessoaJson)
+
 }
 
 func pessoasGetHandler(w http.ResponseWriter, r *http.Request) {
@@ -109,10 +116,40 @@ func pessoasGetHandler(w http.ResponseWriter, r *http.Request) {
             }
         }
 
-        w.WriteHeader(http.StatusNotFound)
         w.Write([]byte("Pessoa not found"))
     } else {
         w.WriteHeader(http.StatusNotFound)
     }
+}
+
+func pessoasSearchHandler(w http.ResponseWriter, r *http.Request) {
+    termo := r.URL.Query().Get("t")
+    if termo == "" {
+        w.WriteHeader(http.StatusBadRequest)
+        w.Write([]byte("Termo de busca não informado"))
+        return
+    }
+
+    var resultados []Pessoa
+    for _, pessoa := range Database {
+        if strings.Contains(pessoa.Apelido, termo) || strings.Contains(pessoa.Nome, termo) {
+            resultados = append(resultados, pessoa)
+            continue
+        }
+        for _, stack := range pessoa.Stack {
+            if strings.Contains(stack, termo) {
+                resultados = append(resultados, pessoa)
+                break
+            }
+        }
+    }
+
+    if len(resultados) > 50 {
+        resultados = resultados[:50]
+    }
+
+    json.NewEncoder(w).Encode(resultados)
+    
+
 }
 
